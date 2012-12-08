babb4b6dfe3f8dbcc0856c5cdb9f848171e4c54b
Peter Eysermans <peter@twototango.be>
2012-12-08 12:49:38 +0100

    Changed port

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.get('/', routes.index);
 
 require('./modules/toilet/routes.js')(app);
 
-app.listen(80, function(){
+app.listen(29378, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
